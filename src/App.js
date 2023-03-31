c605bf71300078a76c8fd99abbe4621d1964f200
raobaba <shrikishunr7@gmail.com>
2023-03-31 09:20:51 +0530

    choosing file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import {useRef} from 'react';
+import {useRef,useState,useEffect} from 'react';
 function App() {
+  const [file,setFile] = useState('');
   const fileInputRef = useRef();
   const url = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
   const onUploadClick = ()=>{
-
+       fileInputRef.current.click();
   }
+  useEffect(()=>{
+
+  },[])
   return (
     <div className="Container">
         <img src={url} alt='logo' />
@@ -17,6 +21,7 @@ function App() {
          type='file' 
          ref={fileInputRef}
          style={{display:'none'}}
+         onChange={(e)=>setFile(e.target.files[0])}
          />
       </div>
     </div>
